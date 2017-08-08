77fb84db8ad4ef962bb49f49e8ada9d4cf95adfd
DangouHaim <gholub1996@mail.ru>
2017-08-08 12:38:10 +0300

    changed var file

--- tasks/variables/var.js
+++ tasks/variables/var.js
@@ -1,8 +1,8 @@
 var res = {
-	themePath: ["./diving/"],
-	fullPath: ["/wp-content/themes/diving/"],
-	siteName: ["diving.com"],
-	siteProxy: ["diving.com:8000"]
+	themePath: ["./site/"],
+	fullPath: ["/wp-content/themes/site/"],
+	siteName: ["site.com"],
+	siteProxy: ["site.com:8000"]
 };
 
 module.exports = res;
